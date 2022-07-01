46003e3c82ddeec9b2aac59507c5256266b09879
naimur <naimur202163@gmail.com>
2022-07-01 14:45:49 +0600

    header added

---
+++ fontend/src/pages/Home/Header/Header.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import logo from "../../../assets/logo.png";
+import "./Header";
+export default function Header() {
+  return (
+    <div className="container mx-auto">
+      <div className="header_content flex justify-between items-center">
+        <div className="logo">
+          <a href="/#">
+            <img
+              className="h-auto max-w-full align-middle"
+              src={logo}
+              alt="logo"
+            />
+          </a>
+        </div>
+        <nav className="header-navbar">
+          <ul className="list-unstyled flex">
+            <li className="header-navbar_item has-sub">
+              <a className="inline-block py-6 px-2.5" href="#/home">
+                <span className="hover:text-amber-50 hover:bg-rose-400 p-1 rounded">Home</span>
+              </a>
+            </li>
+            <li className="header-navbar_item has-sub">
+              <a className="inline-block py-6 px-2.5" href="#/home">
+                <span className="hover:text-amber-50 hover:bg-rose-400 p-1 rounded">PAGE</span>
+              </a>
+            </li>
+            <li className="header-navbar_item has-sub">
+              <a className="inline-block py-6 px-2.5" href="#/home">
+                <span className="hover:text-amber-50 hover:bg-rose-400 p-1 rounded">CLASSES</span>
+              </a>
+            </li>
+            <li className="header-navbar_item has-sub">
+              <a className="inline-block py-6 px-2.5" href="#/home">
+                <span className="hover:text-amber-50 hover:bg-rose-400  p-1 rounded	">TEACHER</span>
+              </a>
+            </li>
+            <li className="header-navbar_item has-sub">
+              <a className="inline-block py-6 px-2.5" href="#/home">
+                <span className="hover:text-amber-50 hover:bg-rose-400 p-1 rounded">BLOG</span>
+              </a>
+            </li>
+            <li className="header-navbar_item has-sub">
+              <a className="inline-block py-6 px-2.5" href="#/home">
+                <span className="hover:text-amber-50 hover:bg-rose-400 p-1   rounded">SHOP</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
+}
---
+++ fontend/src/pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+
+export default function Home() {
+  return (
+    <div>
+        <Header/>
+        <Footer/>
+    </div>
+  )
+}
