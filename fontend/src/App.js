ddeb42f0782302aaf021c70e25801f356ce683c3
naimur <naimur202163@gmail.com>
2022-07-03 19:17:49 +0600

    home Blog added

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home/Home";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Blog from "./pages/Blogs/Blog/Blog";
+import Teacher from "./pages/Teacher/Teacher/Teacher";
 
 function App() {
   AOS.init();
   return (
     <div >
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Blog/>}/>
       </Routes>
    
     </div>
