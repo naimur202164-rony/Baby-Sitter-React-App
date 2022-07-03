e09d408192ab120b4c03197333fd47931aa5e7d3
naimur <naimur202163@gmail.com>
2022-07-03 12:54:55 +0600

    done

--- fontend/src/App.js
+++ fontend/src/App.js
@@ -1,9 +1,13 @@
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home/Home";
 
 function App() {
   return (
     <div >
-    <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+   
     </div>
   );
 }
