bf5afd6d61badb3244dde3b22b8ec7d4d798fda7
naimur <naimur202163@gmail.com>
2022-07-20 11:41:40 +0600

    blog api completed

--- backend/models/blogModel.js
+++ backend/models/blogModel.js
@@ -1,10 +1,21 @@
 const mongoose = require("mongoose");
 
 const blogsSchema = mongoose.Schema({
+  image: {
+    type: String,
+    required: [true, "plesse Enter the Image"],
+  },
   name: {
     type: String,
     required: [true, "Please Enter the Name"],
   },
+  description: {
+    type: String,
+    required: [true, "Please Enter the Description"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please Enter the Price"],
+  },
 });
-
 module.exports = mongoose.model("Blogs", blogsSchema);
