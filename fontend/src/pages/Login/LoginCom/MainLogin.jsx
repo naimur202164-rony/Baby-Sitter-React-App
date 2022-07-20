c3442b901bb5d8c7f2ad3c3854b9725396647660
unknown <Alamin>
2022-07-20 07:32:04 +0600

    login and registration page added

---
+++ fontend/src/pages/Login/LoginCom/MainLogin.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "../../Home/Footer/Footer";
+import Header from "../../Home/Header/Header";
+import Login from "../../Login/Login";
+
+export default function MainLogin() {
+  return (
+    <div>
+      <Header />
+      <Login />
+      <Footer />
+    </div>
+  );
+}
