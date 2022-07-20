6657fe539dc7c11960f3129381c103a77c18765b
naimur <naimur202163@gmail.com>
2022-07-20 17:49:41 +0600

    error solved

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -3,13 +3,12 @@ const {
   getAllBlogs,
   createBlogs,
   blogDetails,
-  updateBlogsDetails,
 } = require("../controllers/blogControler");
 
 const router = express.Router();
 
 router.route("/blogs").get(getAllBlogs);
 router.route("/blogs/new").post(createBlogs);
-router.route("/blogs/:id").get(blogDetails).put(updateBlogsDetails);
+router.route("/blogs/:id").get(blogDetails)
 
 module.exports = router;
